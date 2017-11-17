a2acadaf53bc9f4a06dd7d982553b2bc217e3fb4
Chely Ho <adept_inept@yahoo.com>
2017-11-17 14:00:14 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -7,15 +7,28 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
   constructor(content){
     this.content = content
+  }
+
+  insert(){
+    const self = this                                       //using this below in db.run would actually refer to a different this.... so self is holding current values of this
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+
+    return new Promise(function(resolve){
+      db.run(sql, [self.content], function(err, result){   //passed without err, result as parameters
+        self.id = this.lastID                              //this refers to db
+        resolve(self)
+      })
+    })
+
   }
 
 }
