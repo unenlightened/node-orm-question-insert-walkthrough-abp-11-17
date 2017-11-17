fef4f700f1e5f4fbf5516be1681f490a1797f10d
Chely Ho <adept_inept@yahoo.com>
2017-11-17 14:00:05 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -19,12 +19,12 @@ class Question{
   }
 
   insert(){
-    const self = this
+    const self = this                                       //using this below in db.run would actually refer to a different this.... so self is holding current values of this
     const sql = `INSERT INTO questions (content) VALUES (?)`
 
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){   //passed without err, result as parameters
-        self.id = this.lastID
+        self.id = this.lastID                              //this refers to db
         resolve(self)
       })
     })
